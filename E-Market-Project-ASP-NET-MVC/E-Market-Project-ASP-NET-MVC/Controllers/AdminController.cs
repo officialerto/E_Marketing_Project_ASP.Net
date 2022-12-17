0ebcf3e3c97253cf8468cbf44e50621a72c7bc9f
Ertugrul <57365047+officialerto@users.noreply.github.com>
2022-12-17 14:08:26 +0300

    E-Marketing-Project-ASP-NET-MVC
    E-Marketing-Project-ASP-NET-MVC-C#-SQL-JQUERY-JAVASCRIPT-HTML-CSS-

---
+++ E-Market-Project-ASP-NET-MVC/E-Market-Project-ASP-NET-MVC/Controllers/AdminController.cs
@@ -0,0 +1,49 @@
+﻿using E_Market_Project_ASP_NET_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Market_Project_ASP_NET_MVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        dbemarketingEntities db = new dbemarketingEntities();
+
+        [HttpGet]
+        // GET: Admin
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        // GET: Admin
+        public ActionResult Login(tbl_admin avm)
+        {
+            tbl_admin ad = db.tbl_admin.Where(x => x.ad_username == avm.ad_username && x.ad_password == avm.ad_password).SingleOrDefault();
+            if (ad != null)
+            {
+                Session["ad_id"] = ad.ad_id.ToString();
+                return RedirectToAction("Create");
+            }
+            else
+            {
+                ViewBag.Error = "Invalid username or password";
+            }
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            if (Session["ad_id"]==null)
+            {
+                return RedirectToAction("Login");
+            }
+
+            return View();
+        }
+    }
+
+}
