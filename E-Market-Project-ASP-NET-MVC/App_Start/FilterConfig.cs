52f5bd2465b8635f28907830f50fbf51614f33e9
Ertugrul <57365047+officialerto@users.noreply.github.com>
2022-12-19 16:20:17 +0300

    Add project files.

---
+++ E-Market-Project-ASP-NET-MVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace E_Market_Project_ASP_NET_MVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ E-Market-Project-ASP-NET-MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="E_Market_Project_ASP_NET_MVC.MvcApplication" Language="C#" %>
