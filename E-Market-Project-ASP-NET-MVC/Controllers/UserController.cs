235ed83ca4c1c3076f741c75338b02f8917df1ca
Ertugrul <57365047+officialerto@users.noreply.github.com>
2022-12-20 05:49:46 +0300

    Last_Update_20.12.22_05.49

--- E-Market-Project-ASP-NET-MVC/Controllers/UserController.cs
+++ E-Market-Project-ASP-NET-MVC/Controllers/UserController.cs
@@ -1,54 +1,36 @@
 ﻿using E_Market_Project_ASP_NET_MVC.Models;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using PagedList;
 
 namespace E_Market_Project_ASP_NET_MVC.Controllers
 {
-    public class AdminController : Controller
+    public class UserController : Controller
     {
         dbemarketingEntities db = new dbemarketingEntities();
 
-        [HttpGet]
-        // GET: Admin
-        public ActionResult Login()
+        // GET: User
+        public ActionResult Index(int ?page)
+        {
+            int pagesize = 9, pageindex = 1;
+            pageindex = page.HasValue ? Convert.ToInt32(page) : 1;
+            var list = db.tbl_category.Where(x => x.cat_status == 1).OrderByDescending(x => x.cat_id).ToList();
+            IPagedList<tbl_category> stu = list.ToPagedList(pageindex, pagesize);
+
+            return View(stu);
+        }
+
+        public ActionResult SignUp()
         {
             return View();
         }
 
         [HttpPost]
-        // GET: Admin
-        public ActionResult Login(tbl_admin avm)
-        {
-            tbl_admin ad = db.tbl_admin.Where(x => x.ad_username == avm.ad_username && x.ad_password == avm.ad_password).SingleOrDefault();
-            if (ad != null)
-            {
-                Session["ad_id"] = ad.ad_id.ToString();
-                return RedirectToAction("Create");
-            }
-            else
-            {
-                ViewBag.Error = "Invalid username or password";
-            }
-            return View();
-        }
-
-        public ActionResult Create()
-        {
-            if (Session["ad_id"]==null)
-            {
-                return RedirectToAction("Login");
-            }
-
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Create(tbl_category cvm, HttpPostedFileBase imgfile)
+        public ActionResult SignUp(tbl_user uvm, HttpPostedFileBase imgfile) 
         {
             string path = uploadingfile(imgfile);
             if (path.Equals("-1"))
@@ -57,27 +39,41 @@ namespace E_Market_Project_ASP_NET_MVC.Controllers
             }
             else
             {
-                tbl_category cat = new tbl_category();
-                cat.cat_name = cvm.cat_name;
-                cat.cat_image = path;
-                cat.cat_status = 1;
-                cat.cat_fk_ad =Convert.ToInt32(Session["ad_id"].ToString());
-                db.tbl_category.Add(cat);
+
+                tbl_user u = new tbl_user();
+                u.u_name = uvm.u_name;
+                u.u_email = uvm.u_email;
+                u.u_password = uvm.u_password;
+                u.u_contact = uvm.u_contact;
+                u.u_image = path;
+                db.tbl_user.Add(u);
                 db.SaveChanges();
-                return RedirectToAction("ViewCategory");
+                return RedirectToAction("Login");
+
             }
 
+                return View();
+        }
+        public ActionResult Login()
+        {
             return View();
         }
 
-        public ActionResult ViewCategory(int? page)
+        [HttpPost]
+        // GET: Admin
+        public ActionResult Login(tbl_user avm)
         {
-            int pagesize = 9, pageindex = 1;
-            pageindex = page.HasValue ? Convert.ToInt32(page) : 1;
-            var list = db.tbl_category.Where(x => x.cat_status == 1).OrderByDescending(x => x.cat_id).ToList();
-            IPagedList<tbl_category> stu = list.ToPagedList(pageindex, pagesize);
-
-            return View(stu);
+            tbl_user ad = db.tbl_user.Where(x => x.u_email == avm.u_email && x.u_password == avm.u_password).SingleOrDefault();
+            if (ad != null)
+            {
+                Session["u_id"] = ad.u_id.ToString();
+                return RedirectToAction("CreateAd");
+            }
+            else
+            {
+                ViewBag.Error = "Invalid username or password";
+            }
+            return View();
         }
 
 
@@ -121,6 +117,12 @@ namespace E_Market_Project_ASP_NET_MVC.Controllers
 
             return path;
         }
-    }
 
+        public ActionResult CreateAd()
+        {
+            return View();
+        }
+
+
+    }
 }
